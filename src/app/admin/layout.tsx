82e400c387211aed34abd6b3509ad1ef81c0b8b9
Dhananjay Singh <dhananjay.win2004@gmail.com>
2025-08-28 05:39:09 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <AdminNav>
    
    with t

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,4 +1,4 @@
-import AdminNav from '@/components/AdminNav';
+
 
 export default function AdminLayout({
   children,
@@ -7,7 +7,6 @@ export default function AdminLayout({
 }) {
   return (
     <div className="flex flex-col min-h-screen bg-gray-50">
-      <AdminNav />
       <main className="container py-6 flex-grow">{children}</main>
     </div>
   );
